da01fafed499d0c1a24886e4f2d03aa3dee4452c
Nikita Shapel <shapelnik@mail.ru>
2023-04-08 21:24:24 +0300

    Added name and surname to vendor

--- Online_Marketpace/src/by/fpmibsu/OnlineMarketplace/entity/Vendor.java
+++ Online_Marketpace/src/by/fpmibsu/OnlineMarketplace/entity/Vendor.java
@@ -1,10 +1,13 @@
 package by.fpmibsu.OnlineMarketplace.entity;
 
 public class Vendor {
-    public Vendor(int ID_vendor_, String login_, String password_, String contacts_, int ID_image_) {
+
+    public Vendor(int ID_vendor_, String login_, String password_, String name_, String surname_, String contacts_, int ID_image_) {
         this.ID_vendor_ = ID_vendor_;
         this.login_ = login_;
         this.password_ = password_;
+        this.name_ = name_;
+        this.surname_ = surname_;
         this.contacts_ = contacts_;
         this.ID_image_ = ID_image_;
     }
@@ -33,6 +36,22 @@ public class Vendor {
         this.password_ = password_;
     }
 
+    public String getName_() {
+        return name_;
+    }
+
+    public void setName_(String name_) {
+        this.name_ = name_;
+    }
+
+    public String getSurname_() {
+        return surname_;
+    }
+
+    public void setSurname_(String surname_) {
+        this.surname_ = surname_;
+    }
+
     public String getContacts_() {
         return contacts_;
     }
@@ -52,6 +71,8 @@ public class Vendor {
     private int ID_vendor_;
     private String login_;
     private String password_;
+    private String name_;
+    private String surname_;
     private String contacts_;
     private int ID_image_;
 }
